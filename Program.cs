7782ffdc93f7e1d3b83a5e44d3990489350e0650
A253562 <A253562@LSBSA-9JJCSV3>
2023-12-05 10:56:27 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+            {
+
+                int num = Convert.ToInt32("45");
+                Console.WriteLine(num + 6);
+
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(num1 + num2);
+
+                double num3 = Convert.ToDouble(Console.ReadLine());
+                double num4 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine(num3 + num4);
+            }
+        
+    }
+}
